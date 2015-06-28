864931830a2516f488956c614d7050d72bdc5583
Rasmus <rasmus@norskov.org>
2015-06-28 22:13:24 +0200

    restructured

--- jquery.embed.js
+++ jquery.embed.js
@@ -1,71 +1,63 @@
-(function ($) {
+;(function ($, window, document) {
     $.fn.embed = function (options) {
+        var that = this;
+
         var settings = $.extend({
-            id: this.data('id') || "ScMzIvxBSi4",
-            width: this.data('width') || 640,
-            height: this.data('height') || 360,
-            responsive: this.data('responsive') || true
+            id: this.data('id') || "ScMzIvxBSi4"
         }, options);
 
-        var el = this;
-        var image = new Image(); // initialize empty image
-        var imageURL = '//img.youtube.com/vi/' + settings.id + '/'; // base youtube image url
+        var getThumbnail = function (callback) {
+            var base = '//img.youtube.com/vi/' + settings.id + '/';
+            var sizes = ['maxresdefault', 'hqdefault'];
 
-        // click any element inside the embed container
-        el.find('*').addBack().click(function () {
-            // write iframe with youtube video
-            $(el).html($('<iframe>')
+            (function getImage() {
+                var url = base + sizes[0] + '.jpg';
+
+                $("<img/>").attr('src', url).load(function () {
+                    if (this.width != 120 && this.height != 90) {
+                        if (callback) callback({
+                            url: url,
+                            width: this.width,
+                            height: this.height
+                        })
+                    } else {
+                        sizes.shift();
+                        getImage();
+                    }
+                })
+            })();
+        };
+
+        var setThumbnail = function (url) {
+            that.css('background', 'black url(' + url + ') 50% 50% / cover no-repeat');
+        };
+
+        var setSize = function (width, height) {
+            that.css('height', that.width() / width * height);
+        };
+
+        getThumbnail(function (data) {
+            setThumbnail(data.url);
+
+            setSize(data.width, data.height);
+
+            $(window).resize(function () {
+                setSize(data.width, data.height);
+            })
+        });
+
+        that.find('*').addBack().click(function () {
+            that.html($('<iframe>')
                 .attr('width', '100%')
                 .attr('height', '100%')
                 .attr('src', '//youtube.com/embed/' + settings.id + '?rel=0&amp;autoplay=1&amp;controls=0&amp;showinfo=0')
                 .attr('frameborder', 0))
         });
 
-        // when image is loaded
-        image.onload = function () {
-            // if image is the 404 thumbnail
-            if (this.width == 120 && this.height == 90) {
-                // video does not have HD thumb get default
-                setImage(imageURL.concat('0.jpg'))
-            } else {
-                // video does have a HD thumb
-                setImage(imageURL.concat('maxresdefault.jpg'))
-            }
-
-            setSize();
-        };
-
-        // try max resolution image
-        image.src = imageURL.concat('maxresdefault.jpg');
-
-        function setImage(url) {
-            // set url as background image
-            el.css('background', '#000000 url(' + url + ') 50% 50% / cover no-repeat');
-        }
-
-        function setSize() {
-            // if responsive is enabled
-            if (settings.responsive) {
-                // calculate height from width:height ratio
-                el.css('height', el.width() / settings.width * settings.height);
-            }
-            // if responsive id disabled
-            else {
-                // set width and height
-                el.css({
-                    width: settings.width,
-                    height: settings.height
-                })
-            }
-        }
-
-        // update size on resize
-        $(window).resize(function () {
-            setSize();
-        })
+        return this;
     };
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         $('[data-embed]').embed();
     })
-})(jQuery);
+})(jQuery, window, document);
